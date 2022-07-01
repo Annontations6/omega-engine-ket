5a80368283b4e2dc200c785b06eca582ecfc012a
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-30 23:46:46 -0300

    nice

--- More/SufferControl.js
+++ More/SufferControl.js
@@ -66,6 +66,14 @@ var init = () => {
         c3.getInfo = (amount) => Utils.getMathTo(getDesc(c3.level), getDesc(c3.level + amount));
     }
 
+    // a4
+    {
+        let getDesc = (level) => "a_4=\\sqrt{2^{" + level + "}}";
+        a4 = theory.createUpgrade(6, currency, new FirstFreeCost(new ExponentialCost(1e10, Math.log2(5))));
+        a4.getDescription = (_) => Utils.getMath(getDesc(a4.level));
+        a4.getInfo = (amount) => Utils.getMathTo(getDesc(a4.level), getDesc(a4.level + amount));
+    }
+
     /////////////////////
     // Permanent Upgrades
     theory.createPublicationUpgrade(0, currency, 1e14);
@@ -74,7 +82,7 @@ var init = () => {
 
     ///////////////////////
     //// Milestone Upgrades
-    theory.setMilestoneCost(new LinearCost(15, 30));
+    theory.setMilestoneCost(new LinearCost(15, 20));
 
     {
         a2Exp = theory.createMilestoneUpgrade(0, 2);
@@ -97,6 +105,20 @@ var init = () => {
         c3Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
     }
 
+    {
+        a1Exp = theory.createMilestoneUpgrade(3, 3);
+        a1Exp.description = Localization.getUpgradeIncCustomExpDesc("a_1", "0.1");
+        a1Exp.info = Localization.getUpgradeIncCustomExpInfo("a_1", "0.1");
+        a1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
+    }
+
+    {
+        a4Exp = theory.createMilestoneUpgrade(4, 5);
+        a4Exp.description = Localization.getUpgradeIncCustomExpDesc("a_4", "0.05");
+        a4Exp.info = Localization.getUpgradeIncCustomExpInfo("a_4", "0.05");
+        a4Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
+    }
+
     /////////////////
     //// Achievements
     achievement1 = theory.createAchievement(0, "You Played!", "wowoowo", () => true);
@@ -107,12 +129,14 @@ var init = () => {
 var updateAvailability = () => {
     c1Exp.isAvailable = a2Exp.level > 0;
     c3Exp.isAvailable = a2Exp.level > 0;
+    a1Exp.isAvailable = a2Exp.level > 0;
+    a4Exp.isAvailable = a1Exp.level > 0;
 }
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
-    currency.value += dt * bonus * getA1(a1.level) *
+    currency.value += dt * bonus * getA1(a1.level).pow(getA1Exponent(a1Exp.level)) *
                                    getA2(a2.level).pow(getA2Exponent(a2Exp.level)) *
                                    getA3(a3.level) *
                                    getC1(c1.level).pow(getC1Exponent(c1Exp.level)) *
@@ -122,6 +146,10 @@ var tick = (elapsedTime, multiplier) => {
 
 var getPrimaryEquation = () => {
     let result = "\\dot{\\rho} = a_1";
+
+    if (a1Exp.level == 1) result += "^{1.1}";
+    if (a1Exp.level == 2) result += "^{1.2}";
+    if (a1Exp.level == 3) result += "^{1.3}";
 
     result += "a_2"
 
@@ -145,6 +173,14 @@ var getPrimaryEquation = () => {
     if (c3Exp.level == 1) result += "^{1.2}";
     if (c3Exp.level == 2) result += "^{1.4}";
 
+    result += "a_4"
+
+    if (a4Exp.level == 1) result += "^{1.05}";
+    if (a4Exp.level == 2) result += "^{1.1}";
+    if (a4Exp.level == 3) result += "^{1.15}";
+    if (a4Exp.level == 4) result += "^{1.2}";
+    if (a4Exp.level == 5) result += "^{1.25}";
+
     return result;
 }
 
@@ -163,5 +199,6 @@ var getC3 = (level) => BigNumber.TWO.pow(level).sqrt();
 var getA2Exponent = (level) => BigNumber.from(1 + 0.075 * level);
 var getC1Exponent = (level) => BigNumber.from(1 + 0.1 * level);
 var getC3Exponent = (level) => BigNumber.from(1 + 0.2 * level);
+var getA1Exponent = (level) => BigNumber.from(1 + 0.1 * level);
 
 init();
